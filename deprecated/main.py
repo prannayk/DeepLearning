e30f6094a83d4f8c752b567a62ebf4f668f72db6
Prannay Khosla <prannay.khosla@gmail.com>
2016-08-03 22:09:23 +0530

    Roll out held back, bugs in input, trying manual input and focusing on learning

--- deprecated/main.py
+++ deprecated/main.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import sys
 import tarfile
-from IPython.display import display, Image
+#from IPython.display import display, Image
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
@@ -21,7 +21,7 @@ def download_progress_hook(count, blockSize, totalSize):
 	
 	if last_percent_reported != percent :
 		if percent % 5 == 0:
-			sys.stdout("%s%%"%percent)
+			sys.stdout("%s%%" % percent)
 			sys.stdout.flush()
 		else:
 			sys.stdout.write(".")
@@ -37,9 +37,9 @@ def maybe_download(filename, expected_bytes, force=False):
 	statinfo = os.stat(filename)
 	if statinfo.st_size == expected_bytes:
 		print('Found and verified', filename)
-	else: 
-		raise Exception(
-			'Failed to verify ' + filename + '. Can you get to it with a browser?')
+#	else: 
+#		raise Exception(
+#			'Failed to verify ' + filename + '. Can you get to it with a browser?')
 	return filename
 
 train_filename = maybe_download('notMNIST_large.tar.gz',247336696)
@@ -70,4 +70,49 @@ def maybe_extract(filename, force=False):
 train_folders = maybe_extract(train_filename)
 test_folders = maybe_extract(test_filename)
 
+image_size = 28
+pixel_depth = 255.0
 
+def load_letter(folder, min_num_images):
+	image_files = os.listdir(folder)
+	dataset = np.ndarray(shape=(len(image_files), image_size, image_size),dtype=np.float32)
+	print(folder)
+	num_images = 0
+	for image in image_files:
+		image_file = os.path.join(folder,image)
+		try : 
+			image_data = (ndimage, imread(image_file).astype(float) - pixel_depth/2)/pixel_depth
+			if image_data.shape != (image_size, image_size):
+				raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+			dataset[num_images, :, :] = image_data
+			num_images = num_images + 1
+		except IOError as e:
+			print('Could not read:', image_file, ':', e, '- it\'s ok skipping.')
+
+	dataset = dataset[0:num_images, :, :]
+	if num_images < min_num_images:
+		raise Exception('Many fewer images than expected: %d %d' %(num_images, min_num_images))
+		print('Full dataset tensor:',dataset.shape)
+		print('Mean: ', np.mean(dataset))
+		print('Standard deviation:', np.std(dataset))
+		return dataset
+
+def maybe_pickle(data_folders, min_num_images_per_class, force=False):
+	dataset_names = []
+	for folder in data_folders:
+		set_filename = folder = '.pickle'
+		dataset_names.append(set_filename)
+		if os.path.exists(set_filename) and not force:
+			print('%s already present. Skipping pickling.' % set_filename)
+		else:
+			print('Pickling %s.' %set_filename)
+			dataset = load_letter(folder, min_num_images_per_class)
+			try:
+				with open(set_filename, 'wb') as f:
+					pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+			except Exception as e:
+				print('Unable to save data to', set_filename, ':', e)
+	return dataset_names
+
+train_datasets = maybe_pickle(train_folders, 45000)
+test_datasets = maybe_pickle(test_folders,1800)
